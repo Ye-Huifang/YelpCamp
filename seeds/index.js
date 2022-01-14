c6b91cc60ea64886a369b55974df9d9faa90043f
Huifang-Ye <515108593@qq.com>
2022-01-14 14:53:57 -0500

    Add an author to Campground

--- seeds/index.js
+++ seeds/index.js
@@ -25,6 +25,7 @@ const seedDB = async () => {
         const random1000 = Math.floor(Math.random() * 1000);
         const price = Math.floor(Math.random() * 20) + 10;
         const camp = new Campground({
+            author: '61e1ae5018d0e182cb0df8c2',
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
             title: `${sample(descriptors)} ${sample(places)}`,
             image: 'https://source.unsplash.com/collection/483251',
