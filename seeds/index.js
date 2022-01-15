ab1330840c91a7ba156c3f733d800ea7411d3373
Huifang-Ye <515108593@qq.com>
2022-01-15 16:55:24 -0500

    ready to try deploying

--- seeds/index.js
+++ seeds/index.js
@@ -23,7 +23,7 @@ const sample = array => array[Math.floor(Math.random() * array.length)];
 
 const seedDB = async () => {
     await Campground.deleteMany({});
-    for (let i = 0; i < 50; i++) {
+    for (let i = 0; i < 200; i++) {
         const random1000 = Math.floor(Math.random() * 1000);
         const price = Math.floor(Math.random() * 20) + 10;
         const camp = new Campground({
@@ -32,8 +32,14 @@ const seedDB = async () => {
             title: `${sample(descriptors)} ${sample(places)}`,
             description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam dolores vero perferendis laudantium, consequuntur voluptatibus nulla architecto, sit soluta esse iure sed labore ipsam a cum nihil atque molestiae deserunt!',
             price,
+            geometry: {
+                type: "Point",
+                coordinates: [
+                    cities[random1000].longitude,
+                    cities[random1000].latitude,
+                ]
+            },
             // image: 'https://source.unsplash.com/collection/483251',
-
             images: [
                 {
                     url: 'https://res.cloudinary.com/dmzuriozi/image/upload/v1642218873/YelpCamp/espw9atfz2sfwc0btivu.jpg',
